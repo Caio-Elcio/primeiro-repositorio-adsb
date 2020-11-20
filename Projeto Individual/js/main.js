1ba521b8b26e0869fc59fa5b181a622d89ba15a8
Caio-Elcio <caio.eloy@bandtec.com.br>
2020-11-20 10:33:46 -0300

    att

--- Projeto Individual/js/main.js
+++ Projeto Individual/js/main.js
@@ -1,7 +1,7 @@
 $('.slider-principal').slick({
     lazyLoad: 'ondemand',
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 4000,
 });
